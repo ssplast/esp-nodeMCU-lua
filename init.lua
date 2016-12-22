2151292eb9f7cfa935309f1c85890fc72dfd5254
ssplast <ssplast@i.ua>
2016-12-22 19:46:37 +0200

    asd
    asd

--- init.lua
+++ init.lua
@@ -2,88 +2,88 @@
 -- Станция === esp-ха являеться точкой доступа
 
 G = nil -- чистим всё перед запуском
-Conf = nil -- чистим всё перед запуском
 collectgarbage() -- чистим всё перед запуском
 
-Conf = {
-    reputedPoints = {-- доверенные (ожидаемые) точки
-        -- чтобы не удалять точку из reputedPoints, её можно закоментировать -- :)
-        ["asus_points"] = "пароль",
-        ["точка_доступа_1"] = "пароль",
-        ["точка_доступа_2"] = "пароль",
-        ["точка_доступа_3"] = "пароль",
-        ["25_konteiner"] = "21212122",
-        ["ssplast"] = "21212122"
-    },
-    illPoints = { -- Точки которые надо избегать, к этим точкам подключения не будет.
-        -- У illPoints более высокий приоритет, если точка есть в reputedPoints
-        -- она всё равно будет исключена из возможных к подключению.
-        "asus_points",
-    },
-    wifistaconfig = {-- настройки подключения к точке доступа
-        ip = "192.168.0.111", -- желаемый IP esp-хи как клиента точки [если точка доступа даст DHCP добро] :)
-        netmask = "255.255.255.0",
-        gateway = "192.168.0.1"
-    },
-    wifiapconfig = { -- настройки esp-хи в режиме станции
-        ssid = "ESP-"..node.chipid(), -- имя esp-хи как станции
-        pwd = "88888888", -- пароль авторизации
-        auth = wifi.OPEN, -- режим авторизации wifi.OPEN wifi.WPA_PSK, wifi.WPA2_PSK, wifi.WPA_WPA2_PSK
-        hidden = 0, -- скрытый режим 1(включён) или 0
-        max = 4, -- количество одновременных клиентов (соединений)
-        ip = "192.168.0.100", -- IP esp-хи как станции
-        netmask = "255.255.255.0", -- полезного применения не нашол (просто стандарт)
-        gateway = "192.168.0.1",-- полезного применения не нашол (просто стандарт)
-        start = "192.168.0.111" -- DHCP старт выдачи IP клиентам
-    }
-}
 
-G = {
+
+_G = {
+    conf = {
+        reputedPoints = {-- доверенные (ожидаемые) точки
+            -- чтобы не удалять точку из reputedPoints, её можно закоментировать -- :)
+            ["asus_points"] = "пароль",
+            ["точка_доступа_1"] = "пароль",
+            ["точка_доступа_2"] = "пароль",
+            ["точка_доступа_3"] = "пароль",
+            ["25_konteiner"] = "21212122",
+            ["ssplast"] = "21212122"
+        },
+        illPoints = { -- Точки которые надо избегать, к этим точкам подключения не будет.
+            -- У illPoints более высокий приоритет, если точка есть в reputedPoints
+            -- она всё равно будет исключена из возможных к подключению.
+            "asus_points",
+        },
+        wifistaconfig = {-- настройки подключения к точке доступа
+            ip = "192.168.0.111", -- желаемый IP esp-хи как клиента точки [если точка доступа даст DHCP добро] :)
+            netmask = "255.255.255.0",
+            gateway = "192.168.0.1"
+        },
+        wifiapconfig = { -- настройки esp-хи в режиме станции
+            ssid = "ESP-"..node.chipid(), -- имя esp-хи как станции
+            pwd = "88888888", -- пароль авторизации
+            auth = wifi.OPEN, -- режим авторизации wifi.OPEN wifi.WPA_PSK, wifi.WPA2_PSK, wifi.WPA_WPA2_PSK
+            hidden = 0, -- скрытый режим 1(включён) или 0
+            max = 4, -- количество одновременных клиентов (соединений)
+            ip = "192.168.0.100", -- IP esp-хи как станции
+            netmask = "255.255.255.0", -- полезного применения не нашол (просто стандарт)
+            gateway = "192.168.0.1",-- полезного применения не нашол (просто стандарт)
+            start = "192.168.0.111" -- DHCP старт выдачи IP клиентам
+        }
+    },
     sta = {
         scan = function()
             print("STA Начинаю сканирование.")
             wifi.sta.getap({ssid = nil,bssid = nil,channel = 0,show_hidden = 1}, 1, function(T)
-                G.sta.availablePoints = {}
-                G.sta.reputedPoints = {}
-                G.sta.flagFP = 0
-                G.sta.openPoints = {}
-                G.sta.flagOP = 0
+                _G.sta.availablePoints = {}
+                _G.sta.reputedPoints = {}
+                _G.sta.flagFP = 0
+                _G.sta.openPoints = {}
+                _G.sta.flagOP = 0
                 for b, v in pairs(T) do
                     local s, r, a, c = string.match(v, "([^,]+),([^,]+),([^,]+),([^,]*)")
-                    table.insert(G.sta.availablePoints, {
+                    table.insert(_G.sta.availablePoints, {
                         ssid = s,
                         rssi = r,
                         authmode = a,
                         channel = c,
                         bssid = b
                     })
-                    if Conf.reputedPoints[s] then
-                        table.insert(G.sta.reputedPoints, {
+                    if _G.conf.reputedPoints[s] then
+                        table.insert(_G.sta.reputedPoints, {
                             ssid = s,
-                            pwd = Conf.reputedPoints[s]
+                            pwd = _G.conf.reputedPoints[s]
                         })
                     else
                         if a == 0 then
-                            table.insert(G.sta.openPoints, {ssid = s, pwd = "password"})
+                            table.insert(_G.sta.openPoints, {ssid = s, pwd = "password"})
                         end
                     end
                     print(" " .. b .. "  " .. string.format("%-3s",r) .. "  "..a.."  "..string.format("%-2s",c).."  "..string.format("%-32s",s))
                 end
-                print("STA Обнаружено ".. #G.sta.availablePoints .. " точек, известных: " .. #G.sta.reputedPoints .. ", открытых: " .. #G.sta.openPoints .. ".")
+                print("STA Обнаружено ".. #_G.sta.availablePoints .. " точек, известных: " .. #_G.sta.reputedPoints .. ", открытых: " .. #_G.sta.openPoints .. ".")
 
-                if #G.sta.reputedPoints then -- есть известные точки
+                if #_G.sta.reputedPoints then -- есть известные точки
                     wifi.sta.config(
-                        G.sta.reputedPoints[1].ssid,
-                        G.sta.reputedPoints[1].pwd
+                        _G.sta.reputedPoints[1].ssid,
+                        _G.sta.reputedPoints[1].pwd
                     )
-                elseif #G.sta.openPoints then -- есть открытые точки
+                elseif #_G.sta.openPoints then -- есть открытые точки
                     wifi.sta.config(
-                        G.sta.openPoints[1].ssid,
-                        G.sta.openPoints[1].pwd
+                        _G.sta.openPoints[1].ssid,
+                        _G.sta.openPoints[1].pwd
                     )
                 else -- нет доступных точек
                     print("STA Так как доступных точек нет, следующее сканирование\n\tсетей будет проведено через 5 минут.")
-                    tmr.alarm(0, 1000 * 60 * 5 --[[ 5 минут ]], 1, G.sta.scan)
+                    tmr.alarm(0, 1000 * 60 * 5 --[[ 5 минут ]], 1, _G.sta.scan)
                 end
             end)
         end
@@ -95,11 +95,11 @@ wifi.setmode(wifi.STATIONAP) -- esp-ха будет работать в двух
 wifi.setphymode(wifi.PHYMODE_G)   -- https://nodemcu.readthedocs.io/en/master/en/modules/wifi/#wifisetphymode
 wifi.sta.config("________","________") -- сброс конфига (затирание) 
 wifi.sta.autoconnect(1) -- включаем автоподключение к точке доступа (явно)
-wifi.sta.setip(Conf.wifistaconfig)
+wifi.sta.setip(_G.conf.wifistaconfig)
 
-wifi.ap.setip(Conf.wifiapconfig)
-wifi.ap.config(Conf.wifiapconfig)
-wifi.ap.dhcp.config(Conf.wifiapconfig)
+wifi.ap.setip(_G.conf.wifiapconfig)
+wifi.ap.config(_G.conf.wifiapconfig)
+wifi.ap.dhcp.config(_G.conf.wifiapconfig)
 
 
 
@@ -110,17 +110,16 @@ wifi.eventmon.register(wifi.eventmon.STA_CONNECTED, function(T)
         print("STA потеряно соединение с: "..T.SSID.." BSSID: "..
                 T.BSSID.." reason: "..T.reason)
         wifi.eventmon.unregister(wifi.eventmon.STA_DISCONNECTED)
-        G.sta.scan()
+        _G.sta.scan()
     end)
 
 end)
 wifi.eventmon.register(wifi.eventmon.STA_GOT_IP, function(T)
     print("Получен IP: " .. T.IP)-- T.netmask -- T.gateway
     tmr.stop(0)
-        -- start server dofile
-
+    
 end)
 
-G.sta.scan()
+_G.sta.scan()
 
 
